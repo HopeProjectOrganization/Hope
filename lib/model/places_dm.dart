acc3a35f47e3a770570af74b3fac8c054f03270c
MALAK0244 <malak.hussein0244@icloud.com>
2025-04-14 20:23:07 +0200

    places

--- lib/model/places_dm.dart
+++ lib/model/places_dm.dart
@@ -1,25 +1,36 @@
 class PlaceModel {
+  final int id;
   final String name;
   final String phone;
   final String address;
-  final String? website;
-  final String? logo;
+  final String location;
+  final String website;
+  final String image;
 
   PlaceModel({
+    required this.id,
     required this.name,
     required this.phone,
     required this.address,
-    this.website,
-    this.logo,
+    required this.location,
+    required this.website,
+    required this.image,
   });
 
   factory PlaceModel.fromJson(Map<String, dynamic> json) {
     return PlaceModel(
-      name: json['name'] ?? '',
-      phone: json['phone'] ?? '',
-      address: json['address'] ?? '',
-      website: json['website'],
-      logo: json['logo'],
+      id: json['id'] ?? 0,
+      name: json['hospitalName'] ?? '',
+      phone: json['hospitalNumber'] ?? '',
+      address: json['hospitalAddress'] ?? '',
+      location: json['hospitalLocation'] ?? '',
+      website: json['hospitalWebsite'] ?? '',
+      image: json['logo'] ?? '',
     );
+  }
+
+  @override
+  String toString() {
+    return 'PlaceModel(name: $name, phone: $phone)';
   }
 }
